5adc866db72b636cffa0e07f8e30fbd11dd49a32
Hitoishi Das <hitoishi.das@gmail.com>
2021-06-01 16:44:30 +0530

    Intial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Portfolio`,
+    description: `A website which defines me, Hitoishi Das.`,
+    author: `@dashitoishi23`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
